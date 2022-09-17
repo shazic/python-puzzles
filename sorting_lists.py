ee47335ff6349d9068f50178f4a04bbe784425d5
Shashank Chattopadhyaya <Schattopadhyaya@nextgen.com>
2022-09-17 16:10:18 +0530

    add func to sort list of tuples on a particular element

--- sorting_lists.py
+++ sorting_lists.py
@@ -1,3 +1,5 @@
+import operator
+
 def reverse_sort(iterable_arg):
     """Returns the iterable argument in reverse
 
@@ -8,5 +10,37 @@ def reverse_sort(iterable_arg):
         list: List sorted in reverse
     """
     return sorted(iterable_arg, reverse = True)
+
+def sort_on_element(list_of_tuples, element=1):
+    """Sort a list of tuples based on the second element
+
+    For example, given a list of tuples where each tuple
+    contains ('name', age, height), this function will 
+    sort and return a list in ascending order of age
+    (second element).
+    Example 1:
+    ___________
+    input_list = [('John', 23, 177), ('Ed', 25, 173), ('Al', 21, 168)]
+    print(sort_on_element(input_list))
+    Output:
+    [('Al', 21, 168), ('John', 23, 177), ('Ed', 25, 173)]
+
+    The default behaviour can be modified to use any other
+    element if the second argument is provided.
+    Example 2:
+    ___________
+    input_list = [('John', 23, 177), ('Ed', 25, 173), ('Al', 21, 168)]
+    print(sort_on_element(input_list, 2))
+    Output:
+    [('Al', 21, 168), ('Ed', 25, 173), ('John', 23, 177)]
+
+    Args:
+        - list_of_tuples (list): A list of tuples
+        - element (int, optional): The element number in the tuple on which to sort. Defaults to 1.
+
+    Returns:
+        list: List of sorted tuples
+"""
+    return sorted(list_of_tuples, key=operator.itemgetter(element))
 
 
