41cff4f39e85e37846565092a772c41931c4a89e
Shashank Chattopadhyaya <Schattopadhyaya@nextgen.com>
2022-09-17 16:06:49 +0530

    add function to sort in reverse

---
+++ sorting_lists.py
@@ -0,0 +1,12 @@
+def reverse_sort(iterable_arg):
+    """Returns the iterable argument in reverse
+
+    Args:
+        iterable_arg (list): A list to be sorted
+
+    Returns:
+        list: List sorted in reverse
+    """
+    return sorted(iterable_arg, reverse = True)
+
+
