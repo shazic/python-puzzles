ba893340ca4400fcf74b9f1e026df9694c544c1c
Shashank Chattopadhyaya <Schattopadhyaya@nextgen.com>
2022-09-17 16:13:27 +0530

    add func to return sum of even and odd numbered indeces of a list

--- sorting_lists.py
+++ sorting_lists.py
@@ -73,4 +73,20 @@ def add_lists(list1, list2):
 """
     return list(map(lambda x, y: x + y, numbers1, numbers2))
 
+def even_odd_sums(iterable):
+    """This function that takes a list or tuple of numbers and
+    returns a two-element list, containing (respectively) the
+    sum of the even-indexed numbers and the sum of the
+    odd-indexed numbers.
+    So calling the function as `even_odd_sums([10, 20, 30, 40, 50, 60])`,
+    you’ll get back `[90, 120]`
+
+    Args:
+        iterable (list/tuple of int): An iterable object
+
+    Returns:
+        list: two-element list, containing the sum of the even-indexed numbers
+        and the sum of the odd-indexed numbers
+    """
+    return [sum(iterable[0::2]), sum(iterable[1::2])]
 
